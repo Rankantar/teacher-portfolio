ac9dd3d5fdb8f777a4b8d95bd3d378206aa61a31
nir315 <nir315@gmail.com>
2025-03-07 17:33:09 +0200

    Update courses database

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,28 @@
-#todo: data base, security protocols, schedule, email notification
+#todo: data base, security protocols, schedule, email notification, translate
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
-courses = [
-    {'course_id': 1, 'course_name': 'Infi 1'},
-    {'course_id': 2, 'course_name': 'Infi 2'},
-    {'course_id': 3, 'course_name': 'Linear Algebra'},
-    {'course_id': 4, 'course_name': 'ODE'},
-    {'course_id': 5, 'course_name': 'PDE'},
-    {'course_id': 6, 'course_name': 'Mechanics'},
-    {'course_id': 7, 'course_name': 'Electricity and Magnetism'},
-    {'course_id': 8, 'course_name': 'Modern Physics'},
-    {'course_id': 9, 'course_name': 'Waves'},
-    {'course_id': 10, 'course_name': 'Quantum Mechanics'}
+courses = [#TODO add descriptions and headlines?
+    {'course_id': 1, 'course_name': 'חדו"א 1',
+     'description': 'סדרות\n חקירת פונקציות בעלות נעלם אחד\n טורים'},
+    {'course_id': 2, 'course_name': 'חדו"א 2'},
+    {'course_id': 3, 'course_name': 'אלגברה לינארית'},
+    {'course_id': 4, 'course_name': 'משוואות דיפרנציאליות רגילות'},
+    {'course_id': 5, 'course_name': 'מכניקה קלאסית',
+     'description': 'א. קינמטיקה בקואורדינאטות קרטזיות ובקואורדינאטות פולאריות.\n'
+                    'ב. חוקי ניוטון ותנועת חלקיק נקודתי. פתרון בעיות עם גרביטציה, חיכוך, קפיצים, גלגלות ותנועת מטוטלת.\n'
+                    'ג. תנע קווי ומתקף. בעיות מכניקה עם פליטת או קליטת מסה.\n'
+                    'ד. משפט עבודה ואנרגיה. התנגשויות אלסטיות ואיבוד אנרגיה.\n'
+                    'ה. תנע זוויתי של חלקיק נקודתי. מומנט כוח (טורק). \n'
+                    'ו. חוקי קפלר של תנועת הפלנטות ופיזור קלאסי.\n'
+                    'ז. תנועת גוף קשיח, כאשר כיוון ציר הסיבוב קבוע במרחב. ממונט אינרציה. פתרון בעיות גלגול והחלקה.'},
+    {'course_id': 6, 'course_name': 'חשמל ומגנטיות'},
+    {'course_id': 7, 'course_name': 'פיזיקה מודרנית'},
+    {'course_id': 8, 'course_name': 'גלים'},
+    {'course_id': 9, 'course_name': 'מכניקה קוונטית 1'},
+    {'course_id': 10, 'course_name': 'מתמטיקה לבגרות'},
+    {'course_id': 11, 'course_name': 'פיזיקה לבגרות'}
 ]
 
 students = [
@@ -44,3 +52,7 @@ async def students_info_fist_n(first_n: int  = None):
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
